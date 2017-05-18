67c1b9ffaa370f00eb4eee2eefc6b42f61c5b92b
daniel.faderski <daniel.faderski@jmr.pl>
2017-05-18 19:12:09 +0200

    JSON Api specification

--- validators.py
+++ validators.py
@@ -16,17 +16,18 @@ class AttributeValidator:
     def name(self, name):
         self._name = name
 
-    def validate(self, parent_data):
-        raw_value = self._get_raw_value_from_parent_data(parent_data)
+    def validate(self, parent_data, error_path=None):
+        _, error_path_str = self._generate_error_path_pair(error_path)
 
+        raw_value = self._get_raw_value_from_parent_data(parent_data)
         if self._required and raw_value is None:
-            raise AdapterValidationError('Missing key "%s"' % self._name)
+            raise AdapterValidationError('Missing key "%s"' % error_path_str)
 
         if raw_value is not None and not isinstance(raw_value, self._data_type):
-            raise AdapterValidationError('Incorrect data type for key "%s"' % self._name)
+            raise AdapterValidationError('Incorrect data type for key "%s"' % error_path_str)
 
         if self._required and not raw_value:
-            raise AdapterValidationError('Empty value for key "%s"' % self.name)
+            raise AdapterValidationError('Empty value for key "%s"' % error_path_str)
 
         if raw_value is None:
             return
@@ -36,11 +37,18 @@ class AttributeValidator:
             if k in required_with:
                 required_with.remove(k)
         if required_with:
-            s = 'Attribute "%s" required together with "%s"' % (self._name, ", ".join([k for k in self._required_with]))
+            s = 'Attribute "%s" required together with "%s"' % (error_path_str, ", ".join([k for k in self._required_with]))
             raise AdapterValidationError(s)
 
     def _get_raw_value_from_parent_data(self, parent_data):
         return parent_data.get(self._name, None)
+
+    def _generate_error_path_pair(self, error_path=None):
+        if not error_path:
+            error_path = []
+        error_path_str = "/".join(error_path) + "/" + self._name
+        error_path = error_path + [self.name]
+        return error_path, error_path_str
 
 
 class CompoundedAttributeValidator(AttributeValidator):
@@ -49,13 +57,15 @@ class CompoundedAttributeValidator(AttributeValidator):
         super().__init__(data_type=dict, **kwargs)
         self._child_validators = child_validators
 
-    def validate(self, parent_data):
-        super().validate(parent_data)
+    def validate(self, parent_data, error_path=None):
+        super().validate(parent_data, error_path)
+        error_path, _ = self._generate_error_path_pair(error_path)
+
         raw_value = self._get_raw_value_from_parent_data(parent_data)
         if raw_value is None:
             return
         for child_validator in self._child_validators:
-            child_validator.validate(raw_value)
+            child_validator.validate(raw_value, error_path)
 
 
 class MappingValidationMixin(object):
@@ -63,9 +73,9 @@ class MappingValidationMixin(object):
         super().__init__(**kwargs)
         self._mapping = mapping
 
-    def validate_against_mapping(self, name, raw_value):
+    def validate_against_mapping(self, raw_value, error_path_str):
         if type(raw_value) not in self._mapping:
-            raise AdapterValidationError('Incorrect data type for key "%s"' % name)
+            raise AdapterValidationError('Incorrect data type for key "%s"' % error_path_str)
 
     def get_validator_instance(self, raw_value):
         validator_instance = self._mapping[type(raw_value)]
@@ -75,18 +85,20 @@ class MappingValidationMixin(object):
 
 
 class FreeContentCompoundedAttributeValidator(MappingValidationMixin, CompoundedAttributeValidator):
-    def validate(self, parent_data):
-        super().validate(parent_data)
+    def validate(self, parent_data, error_path=None):
+        super().validate(parent_data, error_path)
+
+        error_path, error_path_str = self._generate_error_path_pair()
         raw_value = self._get_raw_value_from_parent_data(parent_data)
         if raw_value is None:
             return
         child_attributes_names = {child.name for child in self._child_validators}
         for k, v in raw_value.items():
             if k not in child_attributes_names:
-                self.validate_against_mapping(k, v)
+                self.validate_against_mapping(v, error_path_str + '/' + k)
                 validator_instance = self.get_validator_instance(v)
                 validator_instance.name = k
-                validator_instance.validate(raw_value)
+                validator_instance.validate(raw_value, error_path)
 
 
 class FreeTypeAttributeValidator(MappingValidationMixin, AttributeValidator):
@@ -94,15 +106,17 @@ class FreeTypeAttributeValidator(MappingValidationMixin, AttributeValidator):
         kwargs.pop('data_type', None)
         super().__init__(data_type=object, **kwargs)
 
-    def validate(self, parent_data):
-        super().validate(parent_data)
+    def validate(self, parent_data, error_path=None):
+        super().validate(parent_data, error_path)
+
+        error_path, error_path_str = self._generate_error_path_pair(error_path)
         raw_value = self._get_raw_value_from_parent_data(parent_data)
         if raw_value is None:
             return
-        self.validate_against_mapping(self._name, raw_value)
+        self.validate_against_mapping(raw_value, error_path_str)
         validator_instance = self.get_validator_instance(raw_value)
         validator_instance.name = self._name
-        validator_instance.validate(parent_data)
+        validator_instance.validate(parent_data, error_path)
 
 
 class CollectionAttributeValidator(AttributeValidator):
@@ -111,19 +125,20 @@ class CollectionAttributeValidator(AttributeValidator):
         super().__init__(data_type=list, **kwargs)
         self._inner_validator = inner_validator
 
-    def validate(self, parent_data):
-        super().validate(parent_data)
+    def validate(self, parent_data, error_path=None):
+        super().validate(parent_data, error_path)
+
+        error_path, _ = self._generate_error_path_pair(error_path)
         raw_value = self._get_raw_value_from_parent_data(parent_data)
         if raw_value is None:
             return
         self._inner_validator.name = 'collection_item'
         for v in raw_value:
-            #this is hack for preserving the same validators as in whole application
+            #this is hack for preserving the same validator interface as in whole application
             collection_item_parent_data = {
                 'collection_item': v
             }
-            self._inner_validator.validate(collection_item_parent_data)
-
+            self._inner_validator.validate(collection_item_parent_data, error_path)
 
 
 class SchemaValidator:
